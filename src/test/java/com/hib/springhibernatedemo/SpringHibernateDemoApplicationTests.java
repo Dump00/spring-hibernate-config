a22e4550587fa139d3ea0046a469b0adb173e6f9
sovisrushain <sovisrushain@gmail.com>
2023-01-11 18:56:57 +0530

    initial commit

---
+++ src/test/java/com/hib/springhibernatedemo/SpringHibernateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hib.springhibernatedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
